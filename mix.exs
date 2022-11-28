539cc2f3596d67d4752d68b8de18491bb7928b98
Ilya Suzdalnitski <ilya@suzdalnitski.com>
2022-11-27 21:33:30 -0500

    fix: dependencies

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Commanded.EventStore.Adapters.Extreme.Mixfile do
     [
       {:commanded, "~> 1.2"},
       {:extreme, "~> 0.13"},
+      {:elixir_uuid, "~> 1.2.1"},
 
       # Optional dependencies
       {:jason, "~> 1.2", optional: true},
